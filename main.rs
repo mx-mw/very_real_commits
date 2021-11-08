342727bb6a14cf914d50d0f540c4289a1dc90095
mx-mw <87119201+mx-mw@users.noreply.github.com>
2021-11-08 16:20:01 +1100

    lol

---
+++ main.rs
@@ -0,0 +1,10 @@
+use std::process::Command;
+
+fn main() {
+	let output = Command::new("sh")
+		.args(["-c", "git commit -am \"lol\""])
+		.output()
+		.expect("failed to commit :sadge:");
+	
+	println!("{:?}", output.stdout);
+}
