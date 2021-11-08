a343a9d19388814ca357c80ccdd7d12725cdfa4e
mx-mw <87119201+mx-mw@users.noreply.github.com>
2021-11-08 16:24:38 +1100

    lol

--- main.rs
+++ main.rs
@@ -2,22 +2,24 @@ use std::process::Command;
 use std::io::Write;
 
 fn main() {
-	let mut file = std::fs::OpenOptions::new()
-		.write(true)
-		.append(true)
-		.open("test.txt")
-		.unwrap();
+	for i in 0..100 {
+		let mut file = std::fs::OpenOptions::new()
+			.write(true)
+			.append(true)
+			.open("test.txt")
+			.unwrap();
 
-	write!(file, "idk");
+		write!(file, "idk");
 
-	Command::new("sh")
-		.args(["-c", "git add ."])
-		.output()
-		.expect("failed to commit :sadge:");
-	let output = Command::new("sh")
-		.args(["-c", "git commit -am \"lol\""])
-		.output()
-		.expect("failed to commit :sadge:");
-	
-	println!("{}", std::str::from_utf8(&output.stdout).unwrap());
+		Command::new("sh")
+			.args(["-c", "git add ."])
+			.output()
+			.expect("failed to commit :sadge:");
+		let output = Command::new("sh")
+			.args(["-c", "git commit -am \"lol\""])
+			.output()
+			.expect("failed to commit :sadge:");
+		
+		println!("{} {}", std::str::from_utf8(&output.stdout).unwrap(), i);
+	};
 }
